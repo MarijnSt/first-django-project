fc0fd1002e81c59071389836899ece22e03dd9b2
MarijnSt <marijn.stammeleer@gmail.com>
2024-12-12 12:44:51 +0100

    adding styling to polls links

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice    
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
