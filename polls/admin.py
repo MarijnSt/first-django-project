7f45ac3464d059e26946fe7368a94a81411c5b86
MarijnSt <marijn.stammeleer@gmail.com>
2024-12-13 13:41:59 +0100

    add filter and search to question admin list

--- polls/admin.py
+++ polls/admin.py
@@ -7,7 +7,12 @@ class ChoiceInline(admin.TabularInline):
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
+    # list
     list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+    # detail
     fieldsets = [
         (None, {"fields": ["question_text"]}),
         ("Date information", {"fields": ["pub_date"]})
